2fb36e95bcc90e7a1c23a1a1978325abb415d9e7
Mantas <mantuxas001@gmail.com>
2024-08-31 18:52:35 +0300

    Project setup Backend and frontend #1

---
+++ frontend/nuxt.config.ts
@@ -0,0 +1,10 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    compatibilityDate: '2024-04-03',
+    devtools: {enabled: true},
+    runtimeConfig: {
+        public: {
+            baseURL: process.env.API_URL || 'http://client-manager.test/api',
+        },
+    },
+})
---
+++ frontend/pages/index.vue
@@ -0,0 +1,15 @@
+<script setup>
+const { public: { baseURL } } = useRuntimeConfig();
+
+
+</script>
+
+<template>
+  <div>
+    Client manager
+  </div>
+</template>
+
+<style scoped>
+
+</style>
