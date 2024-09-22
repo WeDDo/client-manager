a7467825b6d143203a3c7cc2e4511cec0751dddc
Mantas <mantuxas001@gmail.com>
2024-09-22 22:40:50 +0300

    Confirm on consequential actions #28

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -7,12 +7,22 @@ const {public: {baseURL}} = useRuntimeConfig();
 <template>
     <div>
         <MainMenuBar />
-        <div>
-
+        <div class="m-2">
+            <Card>
+                <template #title>
+                    <div class="text-2xl">
+                        <div>
+                            Welcome!
+                        </div>
+                    </div>
+                </template>
+            </Card>
         </div>
     </div>
 </template>
 
-<style scoped>
-
+<style>
+.p-card .p-card-body {
+    gap: 0;
+}
 </style>
